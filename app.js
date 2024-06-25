79069778271b4152d608bc8ce83bb663fc2aa052
satya-subham <satyasubham85@gmail.com>
2024-06-25 10:50:40 +0530

    origins added

--- app.js
+++ app.js
@@ -19,6 +19,8 @@ app.use(cookieParser());
 app.use(express.json());
 const origins = {
   react: "https://react-mamaearth-clone.vercel.app/",
+  reactVite: "http://localhost:5173/",
+  extra: "http://localhost:5174/"
 };
 app.use(CORS({ origin: origins, credentials: true }));
 
